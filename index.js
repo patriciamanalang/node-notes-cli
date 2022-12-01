285e5e8b764e25b5b9b89a20de2d713f4d245876
Tina Chia <tinachia_5@yahoo.com>
2022-12-01 19:07:23 +0000

    tina grabbed values of properties

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const fs = require('fs');
 const data = require('./data.json');
 const notes = data['notes'];
-// const numbers = notes['1'];
 
 fs.readFile('data.json', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     process.exit(1);
   } else {
-    // console.log(data);
     for (var property in notes) {
-    console.log(property);
+    console.log(notes[property]);
     }
   }
 })
