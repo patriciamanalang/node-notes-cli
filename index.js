792f08672ae72759750a5a0bfd76d2f5f636b8ee
Patricia Manalang <patriciamanalang06@gmail.com>
2022-12-01 18:47:46 +0000

    Patricia accessed values in notes.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const data = require('./data.json');
+const notes = data['notes'];
+// const numbers = notes['1'];
 
 fs.readFile('data.json', 'utf8', (err, data) => {
   if (err) {
@@ -7,8 +9,8 @@ fs.readFile('data.json', 'utf8', (err, data) => {
     process.exit(1);
   } else {
     // console.log(data);
-    for (property in data['notes']) {
-    console.log(data);
+    for (var property in notes) {
+    console.log(property);
     }
   }
 })
