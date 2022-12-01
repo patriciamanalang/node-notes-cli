f6b0a31a8e2b035b58824b1f0cb33b6becf2fb0f
Tina Chia <tinachia_5@yahoo.com>
2022-12-01 20:25:59 +0000

    tina continue to add values

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ const fs = require('fs');
 const data = require('./data.json');
 const notes = data['notes'];
 let nextId = data['nextId'];
+const input = process.argv[3]
 
 
 
-fs.readFile('data.json', 'utf8', (err, data) => {
-  // if (err) {
-  //   console.error(err);
-  //   process.exit(1);
-  // }
+fs.readFile('data.json', 'utf8', (data) => {
   if (process.argv[2] === 'read') {
     for (const property in notes) {
     console.log(`${property}` + ': ' + notes[property]);
     }
   } else if (process.argv[2] === 'add') {
-    data.nextId = process.argv[3];
+    notes.nextId = input;
   }
 
 })
