34365ef9012d746d43e6cc164a4742c3f0b83464
Patricia Manalang <patriciamanalang06@gmail.com>
2022-12-01 20:07:25 +0000

    Patricia started second feature functionality.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const fs = require('fs');
 const data = require('./data.json');
 const notes = data['notes'];
+let nextId = data['nextId'];
+
+
 
 fs.readFile('data.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  } else {
-    for (var property in notes) {
+  // if (err) {
+  //   console.error(err);
+  //   process.exit(1);
+  // }
+  if (process.argv[2] === 'read') {
+    for (const property in notes) {
     console.log(`${property}` + ': ' + notes[property]);
     }
+  } else if (process.argv[2] === 'add') {
+    data.nextId = process.argv[3];
   }
+
 })
