1489aab217e7b9598c28fac140de6629f7a8076e
Noah Lett <noahblett96@gmail.com>
2022-12-01 21:54:23 +0000

    Noah Lett able to add new note

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const fs = require('fs');
 const data = require('./data.json');
 const notes = data['notes'];
-let nextId = data['nextId'];
-const input = process.argv[3]
+// let nextId = data['nextId'];
+const input = process.argv[3];
 
 
-
-fs.readFile('data.json', 'utf8', (data) => {
-  if (process.argv[2] === 'read') {
+if (process.argv[2] === 'read') {
+  fs.readFile('data.json', 'utf8', (data) => {
     for (const property in notes) {
     console.log(`${property}` + ': ' + notes[property]);
     }
+  })
   } else if (process.argv[2] === 'add') {
-    notes.nextId = input;
+  fs.writeFile('data.json', JSON.stringify(process.argv[3], null, 2), 'utf8', (err) =>  {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
   }
-
-})
