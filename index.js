053dfdebfbd3880a0be5d59918bc0d4278b87bcd
Noah Lett <noahblett96@gmail.com>
2022-12-01 18:24:29 +0000

    Noah Lett Created for-in loop to access object

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const data = require('./data.json');
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+fs.readFile('data.json', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     process.exit(1);
   } else {
+    // console.log(data);
+    for (property in data['notes']) {
     console.log(data);
+    }
   }
 })
